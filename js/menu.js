b92dcbfdf44b4d7f2fa26edb3f57b72f8aaa56d3
Laura Campos <lauracampos1411@gmail.com>
2023-03-19 11:00:46 -0300

    Disciplinas cadastradas

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,26 @@
 document.querySelector("#navbar").innerHTML = `
 <header id="navbar"> 
         <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand" href="./index.html">Laura</a>
+            <a class="navbar-brand" href="./index.html">Menu</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Grade horária   
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="./grade.html">2023</a>
-                    <a class="dropdown-item" href="#">2022</a>
-                    <a class="dropdown-item" href="#">2021</a>
-                    <a class="dropdown-item" href="#">2020</a>
-                  </div>
+                <li class="nav-item active">
+                  <a class="nav-link" href="./grade.html">Grade horária</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       Disciplinas   
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">2023</a>
-                      <a class="dropdown-item" href="#">2022</a>
-                      <a class="dropdown-item" href="#">2021</a>
-                      <a class="dropdown-item" href="#">2020</a>
+                      <a class="dropdown-item" href="tecnologias.html">Tecnologias para programação web</a>
+                      <a class="dropdown-item" href="auditoria.html">Auditoria e segurança de sistemas</a>
+                      <a class="dropdown-item" href="redes.html">Redes de computadores II</a>
+                      <a class="dropdown-item" href="software.html">Software livre</a>
+                      <a class="dropdown-item" href="governanca.html">Governaça e tecnologia da informação</a>
+                      <a class="dropdown-item" href="qualidade.html">Qualidade e desenvolvimento de software</a>
                     </div>
                   </li>
               </ul>
